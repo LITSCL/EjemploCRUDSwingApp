e69944c1a8df270d49bd2bfa1b94c18990861d26
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 15:36:23 -0300

    Ok

--- EjemploCRUDSwingApp/SQL-Clear/dbejemplocrudswingapp.sql
+++ EjemploCRUDSwingApp/SQL-Clear/dbejemplocrudswingapp.sql
@@ -1,44 +1,100 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-12-2023 a las 19:10:50
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.0
 
-CREATE SCHEMA IF NOT EXISTS `dbejemplocrudswingapp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `dbejemplocrudswingapp` ;
-
--- -----------------------------------------------------
--- Table `dbejemplocrudswingapp`.`categoria`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbejemplocrudswingapp`.`categoria` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `nombre_UNIQUE` (`nombre` ASC) )
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `dbejemplocrudswingapp`.`producto`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbejemplocrudswingapp`.`producto` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(45) NOT NULL ,
-  `descripcion` VARCHAR(90) NOT NULL ,
-  `precio` DOUBLE NOT NULL ,
-  `stock` INT NOT NULL ,
-  `imagen` VARCHAR(90) NULL ,
-  `categoria_id` INT NOT NULL ,
-  PRIMARY KEY (`id`, `categoria_id`) ,
-  INDEX `fk_producto_categoria_idx` (`categoria_id` ASC) ,
-  CONSTRAINT `fk_producto_categoria`
-    FOREIGN KEY (`categoria_id` )
-    REFERENCES `dbejemplocrud`.`categoria` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `dbejemplocrudswingapp` ;
+--
+-- Base de datos: `dbejemplocrudswingapp`
+--
+CREATE DATABASE IF NOT EXISTS `dbejemplocrudswingapp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dbejemplocrudswingapp`;
 
+-- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `descripcion` varchar(90) NOT NULL,
+  `precio` double NOT NULL,
+  `stock` int(11) NOT NULL,
+  `imagen` varchar(90) DEFAULT NULL,
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre_UNIQUE` (`nombre`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`,`categoria_id`),
+  ADD KEY `fk_producto_categoria_idx` (`categoria_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
